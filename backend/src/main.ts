108f84e4fb30b72cb232aef7164f8b0754d7d324
Rosine22 <r.uwineza@alustudent.com>
2026-03-23 11:41:13 -0600

    updates for front-end deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,12 +5,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const origin = process.env.FRONTEND_URL || 'http://localhost:5173';
+  const origin = [
+    process.env.FRONTEND_URL || 'http://localhost:5173',
+    'https://taskboa.netlify.app/',
+  ];
   app.enableCors({ origin });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`🚀 NestJS API running at port ${port}`);
+  console.log(` NestJS API running at port ${port}`);
 }
 bootstrap();
